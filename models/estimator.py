9aac44a2b596c704245d8b8e49c6ce37b98a1ed3
vinhvu200 <vinhvu200@gmail.com>
2018-01-13 17:42:12 -0800

    Added estimator class

---
+++ models/estimator.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.linear_model import SGDRegressor
+
+
+class Estimator():
+
+    def __init__(self, action_space, init_state):
+        self.models = []
+
+        self.action_space = action_space
+        for _ in range(self.action_space):
+
+            model = SGDRegressor(learning_rate='constant')
+            model.partial_fit([init_state], [0])
+            self.models.append(model)
+
+    def predict(self, state):
+
+        q_vals = np.ones(self.action_space)
+
+        for i in range(len(self.models)):
+            q_vals[i] = self.models[i].predict([state])[0]
+
+        return q_vals
+
+    def update(self, state, action, target):
+        self.models[action].fit([state], [target])
